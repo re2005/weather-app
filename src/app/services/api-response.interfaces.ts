7993378dab99d280dbda8d312d6b2955702c7306
Renato Cardoso <re2005@gmail.com>
2019-10-18 17:32:30 +0200

    feat: added api service

---
+++ src/app/services/api-response.interfaces.ts
@@ -0,0 +1,6 @@
+// Usage of prefix 'I' bellow refer to good practices when creating interfaces
+
+export interface IApiResponse {
+    list: Array<object>;
+    city: object;
+}
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {IApiResponse} from './api-response.interfaces';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ApiService {
+
+    private readonly apiUrl: string = 'http://api.openweathermap.org/data/2.5/';
+    private readonly apiId: string = '&appid=4217e0dc20b9dfb00bc4a701fbaa637c';
+    private readonly units: string = '&units=metric';
+
+    constructor(private http: HttpClient) {
+    }
+
+    public getCitiesWeatherById(ids: Array<number>): Observable<IApiResponse> {
+        const queryIds = ids.join(',');
+        return this.http.get<IApiResponse>(this.apiUrl + 'group?id=' + queryIds + this.apiId + this.units);
+    }
+
+    public getCityForecastById(id: number): Observable<IApiResponse> {
+        return this.http.get<IApiResponse>(this.apiUrl + 'forecast?id=' + id + this.apiId + this.units);
+    }
+
+    public getCitiesByName(name: string): Observable<IApiResponse> {
+        return this.http.get<IApiResponse>(this.apiUrl + 'find?q=' + name + this.apiId);
+    }
+}
