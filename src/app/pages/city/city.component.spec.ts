40553ad8b2cc061a6e91ca327a62b9d7f60b094b
Renato Cardoso <re2005@gmail.com>
2019-10-18 18:11:41 +0200

    refactor: added card and forecast list to city page

--- src/app/pages/city/city.component.spec.ts
+++ src/app/pages/city/city.component.spec.ts
@@ -1,14 +1,32 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 
 import {CityComponent} from './city.component';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 describe('CityComponent', () => {
     let component: CityComponent;
     let fixture: ComponentFixture<CityComponent>;
 
+    const mockRoute: any = {snapshot: {}};
+
     beforeEach(async(() => {
+
         TestBed.configureTestingModule({
-            declarations: [CityComponent]
+            declarations: [CityComponent],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+            providers: [
+                {
+                    provide: HttpClient
+                },
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        params: mockRoute
+                    }
+                }
+            ]
         })
             .compileComponents();
     }));
@@ -16,6 +34,10 @@ describe('CityComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(CityComponent);
         component = fixture.componentInstance;
+
+        mockRoute.parent.params.next({id: 1234});
+        mockRoute.params.next({paramName: 'id'});
+
         fixture.detectChanges();
     });
 
--- src/app/pages/city/city.component.ts
+++ src/app/pages/city/city.component.ts
@@ -1,16 +1,48 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {ApiService} from '../../services/api.service';
+import {takeUntil} from 'rxjs/operators';
+import {CityWeather} from '../../models/city-weather.model';
+import {CityForecast} from '../../models/city-forecast.model';
+import {forkJoin, Subject} from 'rxjs';
+import {faArrowLeft} from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
     selector: 'app-city',
     templateUrl: './city.component.html',
     styleUrls: ['./city.component.scss']
 })
-export class CityComponent implements OnInit {
+export class CityComponent implements OnInit, OnDestroy {
 
-    constructor() {
+    public readonly faArrowLeft = faArrowLeft;
+    private ngUnsubscribe: Subject<void> = new Subject<void>();
+    private id: number;
+    public cityWeather: CityWeather;
+    public cityForecast: Array<CityForecast>;
+
+    constructor(private route: ActivatedRoute,
+                private apiService: ApiService) {
+        this.route.params
+            .subscribe(params => this.id = params.id);
+
     }
 
     ngOnInit() {
+
+        const weatherRequest = this.apiService.getCitiesWeatherById([this.id]);
+        const forecastRequest = this.apiService.getCityForecastById(this.id);
+
+        forkJoin(forecastRequest, weatherRequest)
+            .pipe(takeUntil(this.ngUnsubscribe))
+            .subscribe(([forecast, weather]) => {
+                this.cityWeather = new CityWeather(weather.list[0]);
+                this.cityForecast = forecast.list.map(r => new CityForecast(r));
+            });
     }
 
+    ngOnDestroy(): void {
+        this.ngUnsubscribe.next();
+        this.ngUnsubscribe.complete();
+    }
 }
