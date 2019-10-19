729ece0cd17ae39b0df69a196fe598be750717e1
Renato Cardoso <re2005@gmail.com>
2019-10-19 12:40:59 +0200

    refactor: added navigation for forecast

--- src/app/global/forecast-list/forecast-list.component.ts
+++ src/app/global/forecast-list/forecast-list.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CityForecast} from '../../models/city-forecast.model';
+import {faCaretRight, faCaretLeft} from '@fortawesome/free-solid-svg-icons';
+import {CityWeather} from '../../models/city-weather.model';
 
 @Component({
     selector: 'app-forecast-list',
@@ -9,6 +11,12 @@ import {CityForecast} from '../../models/city-forecast.model';
 export class ForecastListComponent implements OnInit {
 
     @Input() forecast: Array<CityForecast>;
+    @Input() weather: CityWeather;
+
+    public sliceStart = 0;
+    public sliceEnd = 8;
+    public readonly faCaretRight = faCaretRight;
+    public readonly faCaretLeft = faCaretLeft;
 
     constructor() {
     }
@@ -16,4 +24,17 @@ export class ForecastListComponent implements OnInit {
     ngOnInit() {
     }
 
+    next(): void {
+        if (this.sliceEnd < this.forecast.length) {
+            this.sliceStart = this.sliceStart + 8;
+            this.sliceEnd = this.sliceEnd + 8;
+        }
+    }
+
+    prev(): void {
+        if (this.sliceStart > 0) {
+            this.sliceStart = this.sliceStart - 8;
+            this.sliceEnd = this.sliceEnd - 8;
+        }
+    }
 }
